d1b3ccd85260d355cc8c9951e46d215cccbefb5a
uacm-cs <uacm.cs@gmail.com>
2024-02-27 15:39:08 -0600

    update

---
+++ test_ejemplo2.py
@@ -0,0 +1,19 @@
+
+# contenido test_ejemplo2.py
+
+def func(x):
+    return x + 1
+
+
+def sumatoria(lista):
+    s = 0
+    for l in lista:
+        s +=l
+    return s
+
+def test_respuesta():
+    assert func(3) == 5
+
+
+def test_sumatoria():
+    assert sumatoria([1,2,3]) == 6
