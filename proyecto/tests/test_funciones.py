8d7f76a06a4d44dc42aa3246e92f272f7481d057
uacm-cs <uacm.cs@gmail.com>
2024-03-08 16:05:03 -0600

    update

--- proyecto/tests/test_funciones.py
+++ proyecto/tests/test_funciones.py
@@ -1,7 +1,12 @@
 from mi_paquete.mis_funciones import func_cuadratica, func_sumatoria
+import pytest 
 
 def test_func_cuadratica():
     assert func_cuadratica(3) == 9
+    
+    assert func_cuadratica(0.2) == pytest.approx(0.04)
+
+
 
 def test_func_sumatoria():
     assert func_sumatoria([1,2,3]) == 6
