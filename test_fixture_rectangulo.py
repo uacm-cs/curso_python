eff26d5b5c2d3b8f8965ff53bdf243f1e526ee84
uacm-cs <uacm.cs@gmail.com>
2024-03-19 15:02:06 -0600

    update

--- test_fixture_rectangulo.py
+++ test_fixture_rectangulo.py
@@ -17,10 +17,19 @@ def rectangulo():
 
 def test_area(rectangulo):
     # Verifica si el área del rectángulo es 12 (3 * 4)
-    assert rectangulo.area() == 11
+    assert rectangulo.area() == 12
 
 def test_otra_area():
     # También podemos usar la fixture sin pasarla explícitamente
     otro_rectangulo = Rectangulo(5, 6)
     assert otro_rectangulo.area() == 30
+    
+
+@pytest.mark.parametrize("rect, area", [
+    (Rectangulo(2,3), 6),
+    (Rectangulo(4,2), 8)
+    ])
+def test_obj_area(rect, area):
+    # También podemos usar la fixture sin pasarla explícitamente
+    assert rect.area() == area
     
